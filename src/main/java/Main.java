67a52e1230f3efc81c0f4efd2019921763ff1c8f
chiki-andrei <andrei.666.666@mail.ru>
2022-09-08 19:22:24 +0900

    result

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import jdk.dynalink.CallSiteDescriptor;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileOutputStream;
@@ -20,6 +22,9 @@ public class Main {
     private static Row[] rows;
     private static CellStyle cellStyleBorder;
     private static CellStyle cellStyleDefault;
+    private static CellStyle cellStyleCenter;
+    private static CellStyle cellStyleLeft;
+    private static CellStyle cellStyleWrap;
     private static Font font;
     private static Random random;
 
@@ -29,6 +34,9 @@ public class Main {
         rows = new Row[66];
         cellStyleBorder = workbook.createCellStyle();
         cellStyleDefault = workbook.createCellStyle();
+        cellStyleCenter = workbook.createCellStyle();
+        cellStyleLeft = workbook.createCellStyle();
+        cellStyleWrap = workbook.createCellStyle();
         font = workbook.createFont();
         random = new Random();
 
@@ -41,7 +49,7 @@ public class Main {
 
         merge();
         fill();
-        setBorderLayout();
+        setLayout();
 
         workbook.write(new FileOutputStream("kekis.xlsx"));
         workbook.close();
@@ -101,7 +109,7 @@ public class Main {
 
         rows[10].getCell(0).setCellValue("за период с 01.05.2022 по 31.05.2022");
 
-        rows[11].getCell(0).setCellValue("___________________________________________________________________________________________");
+        rows[11].getCell(0).setCellValue("_________________________________________________________________________________________________________________");
 
         rows[12].getCell(0).setCellValue("(сокращенное наименование образовательного учреждения)");
 
@@ -146,31 +154,52 @@ public class Main {
         rows[60].getCell(1).setCellValue("Подписи сторон:");
 
         rows[62].getCell(1).setCellValue("Лицо, ответственное за организацию");
-        rows[62].getCell(4).setCellValue("_________________");
-        rows[62].getCell(7).setCellValue("_________________");
+        rows[62].getCell(4).setCellValue("___________________");
+        rows[62].getCell(7).setCellValue("_________________________");
 
         rows[63].getCell(4).setCellValue("(подпись)");
         rows[63].getCell(7).setCellValue("(Ф.И.О.)");
 
         rows[64].getCell(1).setCellValue("Заведующий производством");
-        rows[64].getCell(4).setCellValue("_________________");
-        rows[64].getCell(7).setCellValue("_________________");
+        rows[64].getCell(4).setCellValue("___________________");
+        rows[64].getCell(7).setCellValue("_________________________");
 
         rows[65].getCell(4).setCellValue("(подпись)");
         rows[65].getCell(7).setCellValue("(Ф.И.О.)");
     }
 
-    private static void setBorderLayout() {
-        cellStyleBorder.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-        cellStyleBorder.setBorderTop(HSSFCellStyle.BORDER_THIN);
-        cellStyleBorder.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-        cellStyleBorder.setBorderRight(HSSFCellStyle.BORDER_THIN);
-
+    private static void setLayout() {
         font.setFontHeightInPoints((short) 10);
         font.setFontName("Times new roman");
 
+        cellStyleBorder.setBorderBottom(XSSFCellStyle.BORDER_THIN);
+        cellStyleBorder.setBorderTop(XSSFCellStyle.BORDER_THIN);
+        cellStyleBorder.setBorderLeft(XSSFCellStyle.BORDER_THIN);
+        cellStyleBorder.setBorderRight(XSSFCellStyle.BORDER_THIN);
+        cellStyleBorder.setAlignment(CellStyle.ALIGN_CENTER);
+        cellStyleBorder.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
         cellStyleBorder.setFont(font);
+
         cellStyleDefault.setFont(font);
+
+        cellStyleCenter.setAlignment(CellStyle.ALIGN_CENTER);
+        cellStyleCenter.setFont(font);
+
+        cellStyleLeft.setAlignment(CellStyle.ALIGN_LEFT);
+        cellStyleLeft.setFont(font);
+        cellStyleLeft.setBorderBottom(XSSFCellStyle.BORDER_THIN);
+
+        cellStyleWrap.setWrapText(true);
+        cellStyleWrap.setAlignment(CellStyle.ALIGN_CENTER);
+        cellStyleWrap.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+        cellStyleWrap.setFont(font);
+
+        rows[2].getCell(8).setCellStyle(cellStyleWrap);
+        rows[2].getCell(9).setCellStyle(cellStyleWrap);
+        cellStyleWrap.setBorderBottom(XSSFCellStyle.BORDER_THIN);
+        cellStyleWrap.setBorderTop(XSSFCellStyle.BORDER_THIN);
+        cellStyleWrap.setBorderLeft(XSSFCellStyle.BORDER_THIN);
+        cellStyleWrap.setBorderRight(XSSFCellStyle.BORDER_THIN);
 
         for (Row row : rows) {
             for (int j = 0; j < 10; j++) {
@@ -186,6 +215,46 @@ public class Main {
         for (int i = 3; i < 10; i++) {
             rows[56].getCell(i).setCellStyle(cellStyleBorder);
         }
+
+        for (int i = 0; i < 10; i++) {
+            rows[14].getCell(i).setCellStyle(cellStyleWrap);
+            rows[15].getCell(i).setCellStyle(cellStyleWrap);
+        }
+
+        rows[4].getCell(7).setCellStyle(cellStyleCenter);
+        rows[4].getCell(8).setCellStyle(cellStyleCenter);
+
+        rows[9].getCell(0).setCellStyle(cellStyleCenter);
+        rows[10].getCell(0).setCellStyle(cellStyleCenter);
+        rows[12].getCell(0).setCellStyle(cellStyleCenter);
+
+        for (int i = 16; i < 56; i++) {
+            rows[i].getCell(3).setCellStyle(cellStyleLeft);
+        }
+
+        rows[63].getCell(4).setCellStyle(cellStyleCenter);
+        rows[63].getCell(7).setCellStyle(cellStyleCenter);
+
+        rows[65].getCell(4).setCellStyle(cellStyleCenter);
+        rows[65].getCell(7).setCellStyle(cellStyleCenter);
+
+        sheet.setColumnWidth(0, 7 * 256);
+        sheet.setColumnWidth(1, 12 * 256);
+        sheet.setColumnWidth(2, 7 * 256);
+        sheet.setColumnWidth(3, 25 * 256);
+        sheet.setColumnWidth(4, 12 * 256);
+        sheet.setColumnWidth(5, 13 * 256);
+        sheet.setColumnWidth(6, 15 * 256);
+        sheet.setColumnWidth(7, 15 * 256);
+        sheet.setColumnWidth(8, 15 * 256);
+        sheet.setColumnWidth(9, 15 * 256);
+
+        rows[2].setHeightInPoints(32);
+        rows[3].setHeightInPoints(32);
+        rows[14].setHeightInPoints(54);
+        rows[15].setHeightInPoints(32);
+        rows[62].setHeightInPoints(32);
+        rows[64].setHeightInPoints(32);
     }
 
 
